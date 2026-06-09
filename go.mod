e036d98916a9c1ff725c398e33fad5d3920701b7
root <liudaoguiguzi@gmail.com>
2026-06-09 23:35:01 +0800

    1

--- go.mod
+++ go.mod
@@ -58,8 +58,6 @@ require (
 	golang.org/x/net v0.48.0
 	golang.org/x/time v0.14.0
 	gopkg.in/ldap.v3 v3.1.0
-	gorm.io/driver/mysql v1.5.7
-	gorm.io/driver/postgres v1.5.9
 	gorm.io/driver/sqlite v1.5.6
 	gorm.io/gorm v1.25.11
 )
@@ -127,7 +125,6 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/hekmon/cunits/v2 v2.1.0 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/klauspost/pgzip v1.2.6 // indirect
 	github.com/matoous/go-nanoid/v2 v2.1.0 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.27
@@ -182,7 +179,6 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/go-webauthn/x v0.1.23 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.3 // indirect
@@ -194,9 +190,6 @@ require (
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/ipfs/go-cid v0.5.0
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
--- internal/bootstrap/db.go
+++ internal/bootstrap/db.go
@@ -10,8 +10,6 @@ import (
 	"github.com/OpenListTeam/OpenList/v4/internal/conf"
 	"github.com/OpenListTeam/OpenList/v4/internal/db"
 	log "github.com/sirupsen/logrus"
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
@@ -46,37 +44,11 @@ func InitDB() {
 		database := conf.Conf.Database
 		switch database.Type {
 		case "sqlite3":
-			{
-				if !(strings.HasSuffix(database.DBFile, ".db") && len(database.DBFile) > 3) {
-					log.Fatalf("db name error.")
-				}
-				dB, err = gorm.Open(openSQLite(fmt.Sprintf("%s?_journal=WAL&_vacuum=incremental",
-					database.DBFile)), gormConfig)
+			if !(strings.HasSuffix(database.DBFile, ".db") && len(database.DBFile) > 3) {
+				log.Fatalf("db name error.")
 			}
-		case "mysql":
-			{
-				dsn := database.DSN
-				if dsn == "" {
-					//[username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
-					dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=%s",
-						database.User, database.Password, database.Host, database.Port, database.Name, database.SSLMode)
-				}
-				dB, err = gorm.Open(mysql.Open(dsn), gormConfig)
-			}
-		case "postgres":
-			{
-				dsn := database.DSN
-				if dsn == "" {
-					if database.Password != "" {
-						dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=Asia/Shanghai",
-							database.Host, database.User, database.Password, database.Name, database.Port, database.SSLMode)
-					} else {
-						dsn = fmt.Sprintf("host=%s user=%s dbname=%s port=%d sslmode=%s TimeZone=Asia/Shanghai",
-							database.Host, database.User, database.Name, database.Port, database.SSLMode)
-					}
-				}
-				dB, err = gorm.Open(postgres.Open(dsn), gormConfig)
-			}
+			dB, err = gorm.Open(openSQLite(fmt.Sprintf("%s?_journal=WAL&_vacuum=incremental",
+				database.DBFile)), gormConfig)
 		default:
 			log.Fatalf("not supported database type: %s", database.Type)
 		}
